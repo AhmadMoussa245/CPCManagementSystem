6c110463c29c5b8dfbc8ee6cdeff961e62cced80
Ahmad Moussa <ahmadmoussa245@gmail.com>
2025-01-03 20:42:46 +0100

    made a solution Model

---
+++ models/solutionModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const solutionSchema=mongoose.Schema(
+    {
+        code:{
+            type:String,
+            required:[true,'code required']
+        },
+        status:{
+            type:String,
+            default:'In Queue',
+            enum:[
+                'In Queue',
+                'Accepted',
+                'Wrong Answer',
+                'Time Limit Exceeded',
+                'Memory Limit Exceeded'
+            ]
+        },
+        createdAt:Date,
+        userId:{
+            type:mongoose.Schema.ObjectId,
+            ref:'User',
+            required:[true,'solution must belong to a user']
+        },
+        problemId:{
+            type:mongoose.Schema.ObjectId,
+            ref:'Problem',
+            required:[true,'solution must belong to a problem']
+        }
+    }
+);
+
+const Solution=mongoose.model('Solution',solutionSchema);
+
+export default Solution;
