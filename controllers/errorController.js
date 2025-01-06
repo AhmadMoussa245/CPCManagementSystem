ba80da38c1d0f0980b15b786dd680941df8b867e
Ahmad Moussa <ahmadmoussa245@gmail.com>
2025-01-06 21:22:00 +0100

    adjusted errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,24 +2,8 @@ export default (err,req,res,next)=>{
     err.statusCode=err.statusCode || 500;
     err.status = err.status || 'error';
 
-    if(err.isOperational){
-        res.status(err.statusCode).json({
-            status:err.status,
-            message:err.message
-        })
-    }else{
-        console.error('ERROR',{
-            name:err.name,
-            message:err.message,
-            expiredAt:err.expiredAt,
-            statusCode:err.statusCode,
-            status:err.status
-        });
-        
-        // send generic message
-        res.status(500).json({
-            status:'error',
-            message:'Something went wrong'
-        });
-    };
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message
+    })
 };
